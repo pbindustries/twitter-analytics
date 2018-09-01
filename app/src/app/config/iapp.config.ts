e5916ae721db200144552f5605b6974c875a30d4
Philip Bankier <pbankier@miso3.com>
2018-08-31 22:51:03 -0400

    boom

--- app/src/app/config/iapp.config.ts
+++
@@ -1,7 +0,0 @@
-export interface IAppConfig {
-  routes: any;
-  votesLimit: number;
-  topTweetersLimit: number;
-  snackBarDuration: number;
-  repositoryURL: string;
-}
--- app/src/app/tweeters/shared/tweeter.model.ts
+++
@@ -1,7 +0,0 @@
-export class Tweeter {
-  constructor(public id: number,
-              public name: string,
-              public handle: string,
-              public followers: number) {
-  }
-}
